3f6a737c475190338e780f679ff37d1a08496277
jojessf <jojessfournier@gmail.com>
2025-02-06 07:08:02 -0500

    set version 0.05

--- Getopt-Lazier/lib/Getopt/Lazier.pm
+++ Getopt-Lazier/lib/Getopt/Lazier.pm
@@ -11,11 +11,11 @@ Getopt::Lazier - Lazy Getopt-like command-line options and argument parser
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 
 =head1 SYNOPSIS
